793740dd98307b5190261e493df74b0c813ef6b9
Arsen <sahalysyk02@mail.ru>
2024-10-03 18:30:32 +0500

    refactor: remove domain/dto/mapper packages

--- camunda-service/src/main/java/com/workflow/mapper/BaseMapper.java
+++
@@ -1,6 +0,0 @@
-package com.workflow.mapper;
-
-public interface BaseMapper<E, D> {
-
-    D map(E entity);
-}
--- camunda-service/src/main/java/com/workflow/mapper/ReadMapper.java
+++
@@ -1,9 +0,0 @@
-package com.workflow.mapper;
-
-import com.workflow.domain.User;
-import com.workflow.dto.ReadUserDto;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface ReadMapper extends BaseMapper<User, ReadUserDto> {
-}
