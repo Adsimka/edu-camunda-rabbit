ca5f8a329d18d1e29ab821f8475008e008529b09
Arsen <sahalysyk02@mail.ru>
2024-10-03 17:55:55 +0500

    add: RabbitConfiguration for mq

---
+++ worker/src/main/java/com/worker/config/RabbitConfiguration.java
@@ -0,0 +1,34 @@
+package com.worker.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfiguration {
+
+    @Value("registration.requests")
+    private String queueName;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+
+        return rabbitTemplate;
+    }
+
+    @Bean
+    Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
